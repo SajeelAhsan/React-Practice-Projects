e353be4c16041a867f3d5a53ebbb573dede1c9ac
Sajeel Ahsan <sajeel.ahsan2298@gmail.com>
2025-01-04 13:25:11 +0100

    Todo project initial setup configured

---
+++ 12-todo-list/src/Todo.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Todo.css'
+
+function Todo () {
+ 
+  return (
+    <>
+      <h1 className="text-3xl ">
+      TODO List
+    </h1>
+    </>
+  )
+}
+
+export default Todo;
